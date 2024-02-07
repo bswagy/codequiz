5ca350268f1630aee9fc2a7083c5171bed756e85
brian <brian.aguilar235@gmail.com>
2024-02-07 14:52:09 -0800

    added css and will continue to add css

--- assets/main.js
+++ assets/main.js
@@ -13,13 +13,13 @@ let score = 0;
 const timeLimitInSeconds = 60; // change as needed
 
 
-const questionEl = document.getElementbyId("question");
-const optionsElc = document.getElementbyId("options");
-const startBtn = document.getElementbyId("start-btn");
-const gameOverel = document.getElementbyId("game-over");
-const finalScoreEl = document.getElementbyId("final-score");
-const initialsInput = document.getElementbyId("initials");
-const submitScoreBtn = document.getElementbyId("submit-score");
+const questionEl = document.getElementById("question");
+const optionsElc = document.getElementById("options");
+const startBtn = document.getElementById("start-btn");
+const gameOverel = document.getElementById("game-over");
+const finalScoreEl = document.getElementById("final-score");
+const initialsInput = document.getElementById("initials");
+const submitScoreBtn = document.getElementById("submit-score");
 
 function startQuiz() {
 startTimer();
@@ -31,9 +31,59 @@ let timeRemaining = timeLimitInSeconds;
 timer = setInterval(() => {
 timeRemaining--;
 if (timeRemaining <= 0) {
-gameOverel();
-}
+gameOver();
+  }
 }, 1000);
 }
+
+function showNextQuestion(){
+const question = questions[currentQuestionIndex];
+
+questionsEl.textContext = question.question;
+optionsEl.innerHTML = "";
+questions.options.forEach(option => {
+   const btn = document.createElement("button");
+btn.textContent = option;
+btn.addEventListener("click", () => checkAnswer(option));
+optionsEl.appendChild(btn);
+});
+
+}
+function checkAnswer(option){
+const question = questions[currentQuestionIndex];
+if (option === question.answer) {
+score++;
+} else {
+    //subtract time if answer is incorrect
+    //adjust time subtraction logic as needed
+}
+currentQuestionIndex++;
+if (currentQuestionIndex < questions.length) {
+showNextQuestion();
+}else {
+    gameOver();
+
+}
+}
+
+function gameOver() {
+clearInterval(timer);
+gameOverEl.style.display = "block";
+finalScoreEl.textContent = score;
+}
+
+startBtn.addEventListener("click", startQuiz);
+submitScoreBtn.addEventListener("click", () => {
+const initials = initialsInput.value.trim();
+//Save initials and score
+
+
+
+});
+
+
+
+
+
 
 
